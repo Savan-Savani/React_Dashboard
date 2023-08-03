2aa6a0139c3f8cd67fc44b9b1dd186b2725d874e
Savan <savansavani111@gmail.com>
2023-08-03 14:40:59 -0400

    vercel analytic remove

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
@@ -9,7 +8,6 @@ ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
-      <Analytics />
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
