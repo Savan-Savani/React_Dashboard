418d142e6001c03e2e86265c95c4b1a5263054f2
Savan <savansavani111@gmail.com>
2023-08-03 14:26:54 -0400

    google analytic add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
@@ -9,6 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
+      <Analytics />
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
